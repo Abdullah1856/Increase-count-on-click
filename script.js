576bbd854937eab706cb0b6f49b6b93ab7566ff2
Abdullah <68245138+Abdullah1856@users.noreply.github.com>
2023-06-14 11:09:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,54 @@ function increase_count() {
     counts = counts + 1
     countbtn.innerText = counts
 }
+
+// let count = 0
+
+// let count_people = document.getElementById("count_txt")
+
+// function increase_count() {
+//     count = count + 1
+//     increase_count()
+// }
+
+
+/*
+let counting = document.getElementById("count_txt")
+
+let count = 0
+function countPeople() {
+    count = count + 1
+    counting.innerText = count
+}
+
+function save() {
+    console.log(count);
+}
+*/
+
+/*
+
+let userName = "Abdullah"
+let message = "You have three new notifications"
+console.log(message + ', ' + userName);
+
+let messageToUser = (message + userName)
+console.log(messageToUser);
+
+let myName = "Abdullah Bin Arshad"
+let greeting = "Hi, my name is"
+
+let myGreeting = (greeting + " " + myName)
+console.log(myGreeting);
+
+*/
+
+/*
+
+let welcomeEl = document.getElementById("welcome-el")
+let Myname = "Abdullah Bin Arshad"
+let greeting = "Hello"
+welcomeEl.innerHTML = greeting + " " + Myname
+
+welcomeEl.innerHTML += " test"
+*/
